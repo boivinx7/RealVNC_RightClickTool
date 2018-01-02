8c581cdbdaa5ab136e492fc713a4e8ef9e9c2c23
boivinx7 <30272385+boivinx7@users.noreply.github.com>
2018-01-02 15:05:05 -0500

    0.9
    Tested and Working
    Still Missing Icons for the Menu
    Install.ps1 and Uninstall.ps1 are working.
    Still need to make MSI installer with AdvanceInstaller and test it

--- Start-RealVNCConnection.ps1
+++ Start-RealVNCConnection.ps1
@@ -15,13 +15,15 @@ try {
 $SCCMSiteCode = Get-WmiObject -Namespace "root\SMS" -Class SMS_ProviderLocation -ComputerName $SiteServer | Select-Object -ExpandProperty SiteCode
 $IPAddresses = Get-WmiObject -Namespace "root\SMS\site_$($SCCMSiteCode)" -Class SMS_R_System -ComputerName $SiteServer -Filter "ResourceID like '$ResourceID'" | Select-Object -ExpandProperty IPAddresses
 }
-
 catch [Exception] {
 $Popup.Popup("Cannot connect to the ConfigMgr-Site: Error-Message: $_.Exception.Message",0,"Error",0)
 Break
 }
+$IP = $IPAddresses[0]
+$Address = $IP + ":0"
+
 try {
-	Start-Process -FilePath "$env:ProgramFiles\RealVNC\VNC Viewer\vncviewer.exe" -ArgumentList "$IPAddresses + ':0'"
+	Start-Process -FilePath "C:\Program Files\RealVNC\VNC Viewer\vncviewer.exe" -ArgumentList $Address
 }
 
 catch [Exception] {
