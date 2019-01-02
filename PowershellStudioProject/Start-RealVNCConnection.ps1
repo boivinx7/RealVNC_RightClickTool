37f85dac29d5101c903b3a52429f83f21f8b89eb
MB_toolz <30272385+boivinx7@users.noreply.github.com>
2019-01-02 14:04:46 -0500

    0.9

--- PowershellStudioProject/Start-RealVNCConnection.ps1
+++ PowershellStudioProject/Start-RealVNCConnection.ps1
@@ -4,6 +4,7 @@ http://www.clientmgmt.de/
 DateCreated: 2016-05-02
 #>
 
+
 param(
     [parameter(Mandatory=$true)]
         $ResourceID,
@@ -11,23 +12,35 @@ param(
         $SiteServer
 )
 
-try {
-$SCCMSiteCode = Get-WmiObject -Namespace "root\SMS" -Class SMS_ProviderLocation -ComputerName $SiteServer | Select-Object -ExpandProperty SiteCode
-$IPAddresses = Get-WmiObject -Namespace "root\SMS\site_$($SCCMSiteCode)" -Class SMS_R_System -ComputerName $SiteServer -Filter "ResourceID like '$ResourceID'" | Select-Object -ExpandProperty IPAddresses
-}
-catch [Exception] {
-$Popup.Popup("Cannot connect to the ConfigMgr-Site: Error-Message: $_.Exception.Message",0,"Error",0)
-Break
-}
-$IP = $IPAddresses[0]
-$Address = $IP + ":0"
+Add-Type -AssemblyName PresentationFramework
 
-try {
-	Start-Process -FilePath "C:\Program Files\RealVNC\VNC Viewer\vncviewer.exe" -ArgumentList $Address
+if (Test-Path -Path "C:\Program Files\RealVNC\VNC Viewer\vncviewer.exe")
+{
+	
+	try
+	{
+		$SCCMSiteCode = Get-WmiObject -Namespace "root\SMS" -Class SMS_ProviderLocation -ComputerName $SiteServer | Select-Object -ExpandProperty SiteCode
+		$IPAddresses = Get-WmiObject -Namespace "root\SMS\site_$($SCCMSiteCode)" -Class SMS_R_System -ComputerName $SiteServer -Filter "ResourceID like '$ResourceID'" | Select-Object -ExpandProperty IPAddresses
+	}
+	catch [Exception] {
+		$Popup.Popup("Cannot connect to the ConfigMgr-Site: Error-Message: $_.Exception.Message", 0, "Error", 0)
+		Break
+	}
+	$IP = $IPAddresses[0]
+	$Address = $IP + ":0"
+	
+	try
+	{
+		Start-Process -FilePath "C:\Program Files\RealVNC\VNC Viewer\vncviewer.exe" -ArgumentList $Address
+	}
+	
+	catch [Exception] {
+		$Popup.Popup("Cannot open VNCViewer: Error-Message: $_.Exception.Message", 0, "Error", 0)
+		Break
+	}
+	
 }
-
-catch [Exception] {
-$Popup.Popup("Cannot open VNCViewer: Error-Message: $_.Exception.Message",0,"Error",0)
-Break
+else
+{
+	[System.Windows.MessageBox]::Show('Could Not Find VNCViewer.exe', 'Check EXE Present', 'OK', 'Error')
 }
-
